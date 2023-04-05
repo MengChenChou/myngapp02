f0f98dfe6e7eabc4f2b3600e4041d0f78c8adec3
MengChenChou <chou53175317@gmail.com>
2023-04-05 13:40:35 +0800

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { UserCenterComponent } from './user-center/user-center.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
+import { InfoComponent } from './info/info.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { SecurityComponent } from './security/security.component';
+
+//聲明路由詞典--路由地址和路由組件的對應集合
+type PathMatch = "full" | "prefix" | undefined;
+const routes = [
+  {path: '', component: IndexComponent},
+  {path: 'index', component: IndexComponent},
+  {path: 'user/center', component: UserCenterComponent},
+  {path: '**', component: NotFoundComponent},
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    UserCenterComponent,
+    NotFoundComponent,
+    InfoComponent,
+    AvatarComponent,
+    SecurityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
